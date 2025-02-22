1191c09cdbcf53e59b5f320c11af08f6379cc77a
felipeclarindo <fglpc2@gmail.com>
2025-02-22 16:44:12 -0300

    chore(dependencies): Configurate the modules changed.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,8 @@
 module com.example {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.base;
 
-    opens com.example to javafx.fxml;
-    exports com.example;
+    opens com.example.controller to javafx.fxml;
+    exports com.example.view;
 }
